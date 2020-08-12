b0d6b979abd50b53f2271fac09ba6d77a90b03ed
dentchan <dentchan@hotmail.com>
2020-08-12 19:54:01 +0800

    Update app.py

--- app.py
+++ app.py
@@ -13,9 +13,15 @@ def index():
 # save the image as a picture
 @app.route('/image', methods=['POST'])
 def image():
+
     i = request.files['image']  # get the image
+    img = cv2.imdecode(numpy.fromstring(i.read(), numpy.uint8), cv2.IMREAD_UNCHANGED)
+ 
     f = ('%s.jpeg' % time.strftime("%Y%m%d-%H%M%S"))
     i.save('%s/%s' % (PATH_TO_TEST_IMAGES_DIR, f))
+    cv2.imshow("hey",img)
+    cv2.waitKey(0)
+
     return Response("%s saved" % f)
 
 # ,ssl_context='adhoc'
